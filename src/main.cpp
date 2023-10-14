b9b12ab5d45a5a8bf4f4c4c21011bb89b4dbcb48
Mohamed Oulmouden <oulmdev@gmail.com>
2023-10-14 18:29:16 +0100

    FEAT: add fillmimetype and token the  configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
 
-<<<<<<< HEAD
 int main (int argc, char **argv) {
-=======
-
-#include "../include/ParseConfig.hpp"
-
-
-	
-int main (int argc, char **argv) {
-
-	if (argc != 2){
-		std::cerr << "No enought arguments" <<	std::endl;
-		return 1;
-	}
-	
-	ParseConfig testParse(argv[1]);	
-
->>>>>>> bb7b01ba9742e9af15822c9f3a830b12bbb02d59
 
 
 }
