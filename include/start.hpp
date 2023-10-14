d2d5d457e339d9e728842a9ccc5ac5b74e3e80c7
Oussama Krich <okrich@e3r3p3.1337.ma>
2023-10-14 12:16:00 +0100

    build: Create file and directories for start

---
+++ include/start.hpp
@@ -0,0 +1,26 @@
+#ifndef START_HPP
+#define START_HPP
+
+
+#include <iostream>
+class start{
+
+
+
+	private:
+
+
+
+	public:
+
+		start();
+		~start();
+		start(start const &copy);
+
+
+		start &operator=(start const &copy);
+
+
+
+};
+#endif
