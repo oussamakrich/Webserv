8d28651a26f8fc181a0a4d01b4f03919d8366d4d
alphaben <bennamrouchex@gamil.com>
2023-11-09 14:42:28 +0100

    add hustler site to projct

---
+++ hustler/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hustler server </title>
+	 <link rel="stylesheet" href="login.css"> 
+</head>
+
+<body>
+        <form method="POST" action="index.php" enctype="application/x-www-form-urlencode" >
+        	<h1> Welcome to  Hustler server </h1>
+        
+        		<input type="text" name="user" placeholder="Enter User Name" value=""><br><br>       	
+        		
+        	    <input type="password" name="pass" placeholder="Enter Password " value=""><br><br>        	
+                <td align="center"><br><input type="submit" value="Log in ">
+        </form>
+<div>
+</div>
+<footer>  
+	Http Hustler Web Server &copy 2023 - 1337 
+ </footer>  
+
+
+ <footer>  
+    Http Hustler Web Server &copy 2023 - 1337 
+ </footer>  
+ 
+</body>
+</html>
+
