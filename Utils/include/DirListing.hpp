da7587dd58d649ac18efc8c3c9938217f6adb7f0
Mohamed Oulmouden <oulmdev@gmail.com>
2023-11-09 16:07:26 +0100

    MERGE: merge upload with get and post

--- Utils/include/DirListing.hpp
+++ Utils/include/DirListing.hpp
@@ -3,17 +3,10 @@
 class DirListing
 {
     private:
-<<<<<<< HEAD
-        static std::string GenerateFileRow(const std::string &parent, const std::string &name);
-        static std::string MakeHtml(std::string DirName, std::string HtmlContent);
-    public :
-       static  bool getDirlistigHtml(const std::string path, std::string &output);
-=======
         static std::string GenerateFileRow(const std::string &parent, const std::string &name ,  std::string vt_path);
         static std::string MakeHtml(std::string DirName, std::string HtmlContent);
     public :
        static  bool getDirlistigHtml(const std::string path, std::string &output,  std::string vt_path);
->>>>>>> origin/mergeGetPost
 };
 
 
